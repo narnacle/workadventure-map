e626d0a3f9cffbefa37c58b857c3df78639b43a1
narnacle <91059545+narnacle@users.noreply.github.com>
2021-10-12 13:28:06 +0300

    Turned jitsiRoomConf into a streamRoom (test 1)

---
+++ tilesets/NarnacleNL/NarnacleNL.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="NarnacleNL" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="Stream-Screen.png" width="160" height="64"/>
+</tileset>
